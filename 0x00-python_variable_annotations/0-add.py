58498dce94f622e9daff66f295b8796458ca5d74
bayelaye313 <bayelaye0510@gmail.com>
2024-10-10 20:03:31 +0000

    first function typed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+def plus(num1: float, num2: float) -> float:
+    """
+        Adds two numbers together.
+
+        Args:
+            num1 (float): The first number.
+            num2 (float): The second number.
+
+        Returns:
+            float: The sum of num1 and num2.
+
+        Example:
+            >>> plus(3.5, 2.5)
+            6.0
+    """
+    return num1 + num2
