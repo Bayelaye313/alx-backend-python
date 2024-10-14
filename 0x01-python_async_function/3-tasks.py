279c5299a4ac05493a4964614eac6a44498e103a
bayelaye313 <bayelaye0510@gmail.com>
2024-10-14 20:40:19 +0000

    return Class TASK

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" The basics of async """
+""" Async basics """
 
-import asyncio
 from asyncio import Task, create_task
-from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay:int)-> Task:
+
+def task_wait_random(max_delay: int) -> Task:
+    """ Tasks """
     task = create_task(wait_random(max_delay))
     return task
