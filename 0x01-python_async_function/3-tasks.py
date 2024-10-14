e903bf52ce49c5994e65dbce05650748df70a6ec
bayelaye313 <bayelaye0510@gmail.com>
2024-10-14 20:34:34 +0000

    fix type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,6 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(int:max_delay)-> Task:
+def task_wait_random(max_delay:int)-> Task:
     task = create_task(wait_random(max_delay))
     return task
