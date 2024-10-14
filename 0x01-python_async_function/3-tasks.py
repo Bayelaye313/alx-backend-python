007af84ceb7e9a1ca691b2bd187046a92461a3b7
bayelaye313 <bayelaye0510@gmail.com>
2024-10-14 20:33:35 +0000

    return class Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" The basics of async """
+
+import asyncio
+from asyncio import Task, create_task
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(int:max_delay)-> Task:
+    task = create_task(wait_random(max_delay))
+    return task
