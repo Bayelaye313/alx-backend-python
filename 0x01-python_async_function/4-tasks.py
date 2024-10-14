11983fe10ce4dd8bf1ce0ebfc0a9381d1a9b144a
bayelaye313 <bayelaye0510@gmail.com>
2024-10-14 20:47:10 +0000

    fix issue

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,5 +11,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawn wait_random n times with the specified max_delay.
     """
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
